14d3f092acfc65edbc798ceaf2328ba38d4f8209
jamiben <jamilazerara@yahoo.fr>
2023-03-16 21:09:53 +0100

    add login service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
+import { LoginComponent } from './Components/login/login.component';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot([
+      {path: 'login', component: LoginComponent}
+      ])
+    ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+

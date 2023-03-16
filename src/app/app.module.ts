6f2fd4e2e3a61d57fe2dc4550d27ec67cdf58186
jamiben <jamilazerara@yahoo.fr>
2023-03-16 21:11:04 +0100

    app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './Components/login/login.component';
+import { HTTP_INTERCEPTORS, HttpClientModule, HttpClientXsrfModule } from '@angular/common/http';
+import {HttpXsrfInterceptorService} from './Interceptors/http-xsrf-interceptor/http-xsrf-interceptor.service'
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        LoginComponent
+    ],
+
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        HttpClientXsrfModule.withOptions({
+            cookieName: 'csrf-token',
+        })
+    ],
+    providers: [
+        {provide: HTTP_INTERCEPTORS, useClass: HttpXsrfInterceptorService, multi:true}
+],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }

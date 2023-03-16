15d6b28b571a73ec818d11f69e5e9b49bfa2c8c0
jamiben <jamilazerara@yahoo.fr>
2023-03-16 21:07:28 +0100

    add import loginService

--- src/app/Components/login/login.component.ts
+++ src/app/Components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from 'src/app/Services/LoginService';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,17 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+    user = {
 
-  constructor() { }
+        username: '',
+        password: '',
+    }
+    wrongCredentials= false;
 
-  userName ='';
-  password='';
-  wrongCredentials=false;
+    constructor(
+        private sessionLogin : LoginService,
+        private router: Router
+    ) { }
 
-  ngOnInit(): void {
-  }
 
-  login():void {
-    this.wrongCredentials = !this.wrongCredentials;
-  }
+    ngOnInit(): void {
+    }
+
+    login():void {
+        this.wrongCredentials = false;
+        this.sessionLogin.login(this.user.username, this.user.password).subscribe(result => {
+            this.router.navigate(['/'])
+        }, error => {
+            this.wrongCredentials = true;
+        })
+
+    }
 }

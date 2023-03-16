db2d6b2673457041db0445ace4034ae249774fa5
jamiben <jamilazerara@yahoo.fr>
2023-03-16 21:09:00 +0100

    add loginService

--- src/app/Services/LoginService.ts
+++ src/app/Services/LoginService.ts
@@ -1,17 +1,47 @@
 import {Injectable} from '@angular/core';
+import {Observable, Subscriber} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
-  privatedIn:'root',
+  providedIn:'root',
 })
 
 export class LoginService {
-    LOGIN_URL='';
-    LOGOUT_URL='';
+    baseUrl: string = environment.baseUrl;
 
-constructor() {}
+    LOGIN_URL='/login';
+    LOGOUT_URL='/logout';
 
-login(pUserName, pPassword){
-private httpClient:
+    constructor(
+        private httpClient: HttpClient
+    ) {}
+    login(pUserName:string, pPassword:string):Observable<boolean>{
+        const loginData = {
+            userName : pUserName,
+            password: pPassword,
+        }
+        return new Observable<boolean>((observer: Subscriber<boolean>) => {
+            this.httpClient.post(environment.baseUrl + this.LOGIN_URL, loginData).subscribe(result => {
+                observer.next(true);
+                observer.complete();
+            }, error => {
+                observer.error(false);
+                observer.complete();
+            });
+        });
+    }
 
+    logout() {
+        return new Observable<boolean>((observer: Subscriber<boolean>) => {
+            this.httpClient.get(environment.baseUrl + this.LOGOUT_URL).subscribe(result => {
+                observer.next(true);
+                observer.complete();
+            }, error => {
+                observer.error(false),
+                observer.complete();
+            });
+        })
+    }
 }
-}
+
